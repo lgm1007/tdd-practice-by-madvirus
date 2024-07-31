6a1217c3e17fdd84566cac17fe9b347780c9db5a
lgm1007 <flxl654@gmail.com>
2024-07-31 20:57:06 +0900

    feat: 프로젝트 최초 생성

---
+++ src/main/kotlin/com/example/tddpracticebymadvirus/TddPracticeByMadvirusApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tddpracticebymadvirus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TddPracticeByMadvirusApplication
+
+fun main(args: Array<String>) {
+    runApplication<TddPracticeByMadvirusApplication>(*args)
+}
