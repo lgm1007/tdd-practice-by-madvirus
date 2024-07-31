cf5994e55ab75eb372bf6843dca346cdf0c73e6a
lgm1007 <flxl654@gmail.com>
2024-07-31 21:29:59 +0900

    test: WEAK 조건 (조건 중 1개만 적용되는 비밀번호) 테스트코드 추가

--- src/test/kotlin/com/example/tddpracticebymadvirus/chap02/PasswordStrengthMeterTest.kt
+++ src/test/kotlin/com/example/tddpracticebymadvirus/chap02/PasswordStrengthMeterTest.kt
@@ -11,6 +11,10 @@ class PasswordStrengthMeterTest {
     1. 길이가 8 이상
     2. 0부터 9 사이의 숫자를 포함
     3. 대문자 포함
+
+    세 조건 모두 충족 시 STRENGTH
+    두 조건 충족 시 NORMAL
+    한 조건 이하로 충족 시 WEAK
      */
 
     @Test
@@ -19,5 +23,29 @@ class PasswordStrengthMeterTest {
         val meter: PasswordStrengthMeter = PasswordStrengthMeter()
         val result: PasswordMeterResult = meter.meter("abc123ABC")
         assertThat(result).isEqualTo(PasswordMeterResult.STRENGTH)
+    }
+
+    @Test
+    @DisplayName("길이가 8 이상인 비밀번호 확인하기")
+    fun meets8LengthConditionStrength() {
+        val meter: PasswordStrengthMeter = PasswordStrengthMeter()
+        val result: PasswordMeterResult = meter.meter("abcdefgh")
+        assertThat(result).isEqualTo(PasswordMeterResult.WEAK)
+    }
+
+    @Test
+    @DisplayName("숫자로만 이루어진 비밀번호 확인하기")
+    fun meetNumbericConditionStrength() {
+        val meter: PasswordStrengthMeter = PasswordStrengthMeter()
+        val result: PasswordMeterResult = meter.meter("123456")
+        assertThat(result).isEqualTo(PasswordMeterResult.WEAK)
+    }
+
+    @Test
+    @DisplayName("대문자로만 이루어진 비밀번호 확인하기")
+    fun meetCapitalLetterConditionStrength() {
+        val meter: PasswordStrengthMeter = PasswordStrengthMeter()
+        val result: PasswordMeterResult = meter.meter("ABCD")
+        assertThat(result).isEqualTo(PasswordMeterResult.WEAK)
     }
 }
