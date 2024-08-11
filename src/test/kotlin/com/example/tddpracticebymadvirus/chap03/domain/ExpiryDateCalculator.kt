1601a261415ec65c598e34156a038d1b49e2abb3
lgm1007 <flxl654@gmail.com>
2024-08-11 11:39:32 +0900

    refactor: calculateExpiryDate() 메서드 리팩토링

--- src/test/kotlin/com/example/tddpracticebymadvirus/chap03/domain/ExpiryDateCalculator.kt
+++ src/test/kotlin/com/example/tddpracticebymadvirus/chap03/domain/ExpiryDateCalculator.kt
@@ -14,20 +14,37 @@ class ExpiryDateCalculator {
                 return plusMonthsBillingDate
             }
 
-            // dayOfMonth: 해당 월의 일을 반환 (1~31)
-            if (payData.firstBillingDate.dayOfMonth != plusMonthsBillingDate.dayOfMonth) {
+            if (!isSameDayOfMonth(payData.firstBillingDate, plusMonthsBillingDate)) {
+                // dayOfMonth: 해당 날짜의 일자를 반환 (1~31)
+                val dayOfFirstBilling: Int = payData.firstBillingDate.dayOfMonth
+                val dayLengthOfExpectExpiryDate: Int = lastDayOfMonth(plusMonthsBillingDate)
+
                 // 예상 만료일이 포함된 달의 마지막 날 < 첫 납부일의 일자
-                if (YearMonth.from(plusMonthsBillingDate).lengthOfMonth() < payData.firstBillingDate.dayOfMonth) {
+                if (dayLengthOfExpectExpiryDate < dayOfFirstBilling) {
                     // 예상 만료일의 마지막 날로 일자를 조정
                     return plusMonthsBillingDate.withDayOfMonth(
-                        YearMonth.from(plusMonthsBillingDate).lengthOfMonth()
+                        dayLengthOfExpectExpiryDate
                     )
                 }
                 // withDayOfMonth(int day): day 값을 일로 받은 날짜로 변환
-                return plusMonthsBillingDate.withDayOfMonth(payData.firstBillingDate.dayOfMonth)
+                return plusMonthsBillingDate.withDayOfMonth(dayOfFirstBilling)
             }
         }
 
         return plusMonthsBillingDate
+    }
+
+    /**
+     * 두 날짜가 같은 일수를 가지고 있는 달인지 확인하기
+     */
+    private fun isSameDayOfMonth(date1: LocalDate, date2: LocalDate): Boolean {
+        return date1.dayOfMonth == date2.dayOfMonth
+    }
+
+    /**
+     * 달의 마지막 일자를 구하는 메서드
+     */
+    private fun lastDayOfMonth(date: LocalDate): Int {
+        return YearMonth.from(date).lengthOfMonth()
     }
 }
