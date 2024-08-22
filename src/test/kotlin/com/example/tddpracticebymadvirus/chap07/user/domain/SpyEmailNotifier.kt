d038a52550259f6802d2ffdfda0d158bf14796a5
lgm1007 <flxl654@gmail.com>
2024-08-22 21:45:33 +0900

    test: (ex. 7.38) assertEquals 검증 통과를 위한 작업

--- src/test/kotlin/com/example/tddpracticebymadvirus/chap07/user/domain/SpyEmailNotifier.kt
+++ src/test/kotlin/com/example/tddpracticebymadvirus/chap07/user/domain/SpyEmailNotifier.kt
@@ -2,9 +2,10 @@ package com.example.tddpracticebymadvirus.chap07.user.domain
 
 class SpyEmailNotifier(
     var called: Boolean,
-    val email: String?,
+    var email: String?,
 ) : EmailNotifier {
     override fun sendRegisterEmail(email: String) {
         called = true
+        this.email = email
     }
 }
