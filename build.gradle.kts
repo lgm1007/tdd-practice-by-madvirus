19cd7d0b15341fd04d299670c0d735ea7fb5e96c
lgm1007 <flxl654@gmail.com>
2024-08-28 21:34:07 +0900

    build: Spring jdbc, jpa 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
