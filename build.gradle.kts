63c2a7c46a0ab75008058ee051dfbcf31cf89e40
lgm1007 <flxl654@gmail.com>
2024-08-28 21:45:53 +0900

    build: kotlin-jpa 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("io.freefair.lombok") version "8.1.0"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
+    kotlin("plugin.jpa") version "1.9.24"
     kotlin("plugin.lombok") version "2.0.10"
 }
 
